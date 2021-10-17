ef6146fb39c327d0e73a292b061bebdb3847b3d2
Christian Salomonsen <55956280+salomaestro@users.noreply.github.com>
2021-10-17 21:16:26 +0200

    created a test subdirectory within src

---
+++ gifconverter/src/tests/test_making_gif.py
@@ -0,0 +1,10 @@
+import os
+
+from gifconverter import make_gif
+
+path = os.path.dirname(__file__)
+img_folder = os.path.join(path, "test_images")
+
+save_as = os.path.join(path, "giftest.gif")
+
+make_gif(img_folder, save_as)
