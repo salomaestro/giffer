988753a12cb6cc9e60155da6165c8d0779f0a7aa
Christian Salomonsen <55956280+salomaestro@users.noreply.github.com>
2021-10-17 21:16:54 +0200

    current main package function

---
+++ gifconverter/src/_util.py
@@ -0,0 +1,25 @@
+import imageio
+import os
+
+def make_gif(folder_name: str, save_as: str) -> None:
+    """ 
+    Makes a gif from a collection of png images in an array where image names are of the form: img (i).png for i = 0, i -> N.
+
+    Parameters:
+        folder_name: string
+            Name of the folder containing images which are to be converted into a gif.
+        save_as: string
+            Name of final gif file, with absolute path. Must be of the form: ../my_video.gif
+    """
+
+    filenames = ["img (" + str(i) + ").png" for i, _ in enumerate(os.listdir(folder_name))]
+    images = []
+    for filename in filenames:
+        images.append(imageio.imread(os.path.join(folder_name, filename)))
+
+    imageio.mimsave(save_as, images)
+
+if __name__ == "__main__":
+    cwd = os.getcwd()
+
+    save_as = "new_gif.gif"
